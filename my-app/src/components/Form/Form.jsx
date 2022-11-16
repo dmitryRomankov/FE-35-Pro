8cd20b0a01799bdb7301945591311886cc9f3dcc
dmitryRomankov <strmr@tut.by>
2022-11-16 23:15:08 +0300

    Code from lesson-38

--- my-app/src/components/Form/Form.jsx
+++ my-app/src/components/Form/Form.jsx
@@ -21,7 +21,6 @@ export class Form extends Component {
           <h2>Inputs</h2>
           {children}
         </div>
-        <Button onClick={this.handleSubmitForm} />
       </form>
     );
   }
