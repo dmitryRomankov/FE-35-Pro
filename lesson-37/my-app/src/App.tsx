59375b33dfb12252d9d321a5a9a6de3d3ec26c12
antonsoloviev <led_soloviev@mail.ru>
2022-11-20 19:07:03 +0300

    feat (hw-37):add css

--- lesson-37/my-app/src/App.tsx
+++ lesson-37/my-app/src/App.tsx
@@ -1,31 +1,24 @@
 import React, { useState } from 'react';
-import {Button} from './components/Button';
-import {Welcome} from './pages/Welcome'
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+
 
 const Input = ({ handleChange, value }) => <input onChange={handleChange} defaultValue={value} type="text" />;
 
 function App() {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setValue(value);
-  };
 
   return (
     <>
-      <div>My app</div>
-      <Button
-        color='red'
-        onHandleClick={() => console.log('Clicked!')}
-        text='Edit'
-        disabled={value?.length < 3}
-        >
-        <div>Edit</div>
-      </Button>
-      <Button text="Save" onHandleClick={() => console.log('Clicked!')} />
-      <Input handleChange={handleChange} value={value}/>
-      <Welcome arr={[1, 2, 3]} text="Some text" title="Some Title" content="Some content"/>
+      <Header></Header>
+
+      <Main />
+      
+
+      <Footer />
+
     </>
   );
 }
