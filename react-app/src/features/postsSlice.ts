f34875eff71a44546ac76af01d308dba96aee579
PyrkinValentin <you@example.com>
2022-12-20 17:54:05 +0300

    init hw 46

--- react-app/src/features/postsSlice.ts
+++ react-app/src/features/postsSlice.ts
@@ -1,10 +1,12 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit"
 import {getPosts} from "./postsThunk"
+import {getPost} from "./postThunk"
 
-import {IGetPosts, IPostsInitialState} from "../interfaces"
+import {IPosts, IPost, IPostsInitialState} from "../interfaces"
 
 const initialState: IPostsInitialState = {
 	posts: [],
+	post: undefined,
 	favorites: [],
 	popular: [],
 	likes: {},
@@ -54,14 +56,23 @@ const postsSlice = createSlice({
 		builder.addCase(getPosts.pending, (state) => {
 			state.loading = true
 		})
-		builder.addCase(
-			getPosts.fulfilled,
-			(state, action: PayloadAction<IGetPosts>) => {
-				state.loading = false
-				state.posts = action.payload.results
-			}
-		)
+		builder.addCase(getPosts.fulfilled, (state, action: PayloadAction<IPosts>) => {
+			state.loading = false
+			state.posts = action.payload.results
+		})
 		builder.addCase(getPosts.rejected, (state, action: any) => {
+			state.loading = false
+			state.error = action.payload
+		})
+
+		builder.addCase(getPost.pending, (state) => {
+			state.loading = true
+		})
+		builder.addCase(getPost.fulfilled, (state, action: PayloadAction<IPost>) => {
+			state.loading = false
+			state.post = action.payload
+		})
+		builder.addCase(getPost.rejected, (state, action: any) => {
 			state.loading = false
 			state.error = action.payload
 		})
